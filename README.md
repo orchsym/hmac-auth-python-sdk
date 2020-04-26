737a67672acb3dfd5b3d2c9127d62290479b229a
Shuoqing Ding <dsq704136@gmail.com>
2020-04-26 15:29:48 +0800

    refactor: support full URL passed in, and improved readme

--- README.md
+++ README.md
@@ -1,38 +1,85 @@
 # HMAC认证SDK使用说明
 
-hmac认证 implemented by python.
+Python版本的SDK，用于生成Orchsym API Gateway HMAC 认证所需的请求头。
 
-### 简介  
+支持 Python2.7+。
+
+### HMAC简介
+
 * hmac是Hashing for Message Authentication的简写，可以用来保证数据的完整，客户端把内容通过散列/哈希算法算出一个摘要，并把算法和内容以及摘要传送给服务端，服务端按照这个算法也算一遍，和摘要比一下如果一样就认为内容是完整的，如果不一样就认为内容被篡改了。
 
 ### 计算签名
-1). 计算queryParam Content-md5.  
-2). 构建待签名报文String 
-    sig_list.append("X-Date: ")
-    sig_list.append(signature_headers["X-Date"])
-    sig_list.append("\n")
-    sig_list.append("Content-md5: ")
-    sig_list.append(str(signature_headers["Content-md5"], encoding = "utf8"))
-    sig_list.append("\n")
-    sig_list.append(signature_headers["request-line"])
-    "".join(sig_list)  
-3). 构建签名     h = hmac.new(bytes(secret, encoding = "utf8"), (string_to_hash).encode("utf-8"), hashlib.sha256)
-    return base64.b64encode(h.digest())
 
-### 构建request报文：
-设置header：  
+1. 生成 `X-Date`、`Content-md5` 参数
 
-```   
+2. 构建待签名报文字符串 
+
+```
+sig_list.append("X-Date: ")
+sig_list.append(signature_headers["X-Date"])
+sig_list.append("\n")
+sig_list.append("Content-md5: ")
+sig_list.append(signature_headers["Content-md5"].decode("utf8"))
+sig_list.append("\n")
+sig_list.append(signature_headers["request-line"])
+"".join(sig_list)
+```
+
+3. 对签名字符串使用SHA256算法进行HMAC加密
+
+```
+h = hmac.new(bytes(secret.decode("utf8")), (string_to_hash).encode("utf-8"), hashlib.sha256)
+return base64.b64encode(h.digest())
+```
+
+
+
+### 构建请求头报文
+
+设置header:
+
+```
   Authorization:hmac username="userhmac", algorithm="hmac-sha256", headers="X-Date Content-md5 request-line", signature="p4sGy3B+J/Zqt7gaLJVZCzVY5/Y="
   X-Date:Mon, 31 Jul 2017 07:25:07 GMT
   Content-md5:IgWlVHazOsGgHGVlcKvQDA==
 ```
-### 方法说明：
-获取请求必要头信息:  
-方法：`generate_request_headers`
-|  参数名称  | 说明  |
-|  ----  | ----  |  
-| username  | hmac用户名 |  
-| secret  | hmac秘钥 |  
-| request_method  | 请求方法 |  
-| url  | 请求path |   
+
+### 方法说明
+
+#### generate_request_headers(username, secret, request_method, url)
+
+描述：根据给定请求信息以及HMAC用户名、秘钥，生成HMAC认证所需的请求头。
+
+参数：
+
+- username: HMAC 认证用户名
+
+- secret: HMAC 认证秘钥
+
+- request_method: 请求方法
+
+- url: 请求完整路径，比如: `http://www.example.com/a/b/c?type=1`
+
+
+返回:
+
+```
+{
+  'Content-md5': '98mUgq1pd7snmumXmFAtLQ==',
+  'Authorization': 'hmac username="user_tom",algorithm="hmac-sha256",headers="X-Date Content-md5 request-line",signature="Pzsm3qYCnnuq+ZKz7rJ84Gl83ZRM/Sb6RQ2XiUPZWxc="',
+  'X-Date': 'Sun, 26 Apr 2020 06:56:08 GMT'
+}
+```
+
+### 使用示例
+
+```
+    url = "https://172.18.28.240/env-101/por-1/test/api/users/2"
+    headers = generate_request_headers("user_tom", "password", "GET", url)
+
+    import requests
+    r = requests.get(url, verify=False, headers=headers)
+    print(r.text)
+```
+
+
--- gateway_hmac.py
+++ gateway_hmac.py
@@ -5,9 +5,11 @@ import base64
 import hashlib
 import hmac
 import re
-from wsgiref.handlers import format_date_time
+
 from datetime import datetime
 from time import mktime
+from urlparse import urlparse
+from wsgiref.handlers import format_date_time
 
 
 def create_date_header():
@@ -29,7 +31,7 @@ def get_signature_string(signature_headers):
     sig_list.append(signature_headers["X-Date"])
     sig_list.append("\n")
     sig_list.append("Content-md5: ")
-    sig_list.append(str(signature_headers["Content-md5"], encoding = "utf8"))
+    sig_list.append(signature_headers["Content-md5"].decode("utf8"))
     sig_list.append("\n")
     sig_list.append(signature_headers["request-line"])
     return "".join(sig_list)
@@ -40,24 +42,53 @@ def md5_hash_base64(string_to_hash):
     return base64.b64encode(m.digest())
 
 def sha256_hash_base64(string_to_hash, secret):
-    h = hmac.new(bytes(secret, encoding = "utf8"), (string_to_hash).encode("utf-8"), hashlib.sha256)
+    h = hmac.new(bytes(secret.decode("utf8")), string_to_hash.encode("utf8"), hashlib.sha256)
     return base64.b64encode(h.digest())
 
+def extract_path(url):
+    url_object = urlparse(url)
+    path = url_object.path
+
+    # the env prefix of URI, like /env-101, is used for gateway environment proxy,
+    # and will not be used in HMAC signature computation.
+    return re.sub(r'^/env-\d+/', '/', path)
+
+
 def generate_request_headers(username, secret, request_method, url):
+    """
+    Generate request headers which will be required in HMAC authorization.
+
+    Parameters:
+    - username (string): HMAC username
+    - secret (string): HMAC secret 
+    - request_method (string): the  method of request to be sent, like 'GET', 'POST'
+    - url (string): the URL of the request to be sent. 
+
+    Returns:
+    dict: Headers which are required in request authorization
+    """
+
     auth_header_template = (
         'hmac username="{}",algorithm="{}",headers="{}",signature="{}"'
     )
     algorithm = "hmac-sha256"
+    headers = "X-Date Content-md5 request-line"
+
     date_header = create_date_header()
     signature_headers = {}
+    path = extract_path(url)
+
     signature_headers["X-Date"] = date_header
-    base64md5 = md5_hash_base64(url.encode("utf8"))
+    base64md5 = md5_hash_base64(path.encode("utf8"))
+
     signature_headers["Content-md5"] = base64md5
-    request_line = request_method + " " + url
+    request_line = request_method.upper() + " " + path
+
     signature_headers["request-line"] = request_line
-    headers = "X-Date Content-md5 request-line"
     signature_string = get_signature_string(signature_headers)
+
     signature_hash = sha256_hash_base64(signature_string, secret)
+
     auth_header = auth_header_template.format(
         username, algorithm, headers, signature_hash
     )
@@ -69,3 +100,13 @@ def generate_request_headers(username, secret, request_method, url):
     }
 
     return request_headers
+
+if __name__ == "__main__":
+    url = "https://172.18.28.240/env-101/por-1/test/api/users/2"
+    headers = generate_request_headers("tom", "passw0rd", "GET", url)
+
+    print(headers)
+
+    # import requests
+    # r = requests.get(url, verify=False, headers=headers)
+    # print(r.text)
